87f41fbb7d48d18f75b820a11c6bbfb2d0031193
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2025-02-04 23:20:28 +0200

    viikko2 pari ekaa

---
+++ listan_tehokkuus_1.py
@@ -0,0 +1,23 @@
+import time
+
+lista = []
+
+def lisaa(lista, n):
+    for i in range(n):
+        lista.append(i)
+
+def poista(lista, n):
+    for i in range(n):
+        lista.pop()
+
+
+alku = time.time()
+_ = lisaa(lista, 1000000)
+loppu = time.time()
+print(f"time: {loppu - alku:.6f} s")
+
+
+alku = time.time()
+_ = poista(lista, 1000000)
+loppu = time.time()
+print(f"time: {loppu - alku:.6f} s")
